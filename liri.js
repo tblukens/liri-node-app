1aed77d378fe8ea6884f73bf0d433b08606f652a
Tim L <tblukens87@gmail.com>
2018-10-10 18:41:12 -0500

    Added chalk module to brighten up the console a bit.

--- liri.js
+++ liri.js
@@ -11,6 +11,8 @@ var spotify = new Spotify(keys.spotify);
 var request = require('request');
 // moment to format dates
 var moment = require('moment');
+// Use chalk to add a bit of style to the command line
+const chalk = require('chalk');
 //remove node and file path from process.argv
 process.argv.splice(0, 2);
 //get the command 
@@ -29,6 +31,7 @@ switch (command) {
         if (argInfo == "") {
             console.log("Please type an artist or bands name after the concert-this command.")
         } else {
+            console.log(chalk.bgYellowBright.blue("Okay, running "+command.toUpperCase()+" searching for "+argInfo.toUpperCase()))
             concertThis(argInfo);
         }
         break;
@@ -36,8 +39,11 @@ switch (command) {
     case 'spotify-this-song':
         // if no song is input into args then default to the sign...
         if (argInfo == "") {
-            spotifyThisSong("The Sign Ace of Base");
+            argInfo = "The Sign Ace of Base";
+            console.log(chalk.bgYellowBright.blue("Okay, running "+command.toUpperCase()+" searching for "+argInfo.toUpperCase()))
+            spotifyThisSong(argInfo);
         } else {
+            console.log(chalk.bgYellowBright.blue("Okay, running "+command.toUpperCase()+" searching for "+argInfo.toUpperCase()))
             spotifyThisSong(argInfo);
         }
         break;
@@ -45,7 +51,9 @@ switch (command) {
 
     case 'movie-this':
         if (argInfo == "") {
-            movieThis("Mr. Nobody");
+            argInfo = "Mr. Nobody";
+            console.log(chalk.bgYellowBright.blue("Okay, running "+command.toUpperCase()+" searching for "+argInfo.toUpperCase()))
+            movieThis(argInfo);
         } else {
             movieThis(argInfo);
         }
@@ -60,6 +68,8 @@ switch (command) {
             var dataArr = data.split(",");
             var txtCommand = dataArr[0];
             var txtInput = dataArr[1];
+            
+            console.log(chalk.bgYellowBright.blue("Okay, running "+txtCommand+" searching for "+txtInput.toUpperCase()))
 
             doWhatItSays(txtCommand, txtInput)
         })
@@ -67,7 +77,7 @@ switch (command) {
 
 
     default:
-        console.log("Please use one of the following commands:\nconcert-this (artist)\nspotify-this-song (song)\nmovie-this (movie name)\ndo-what-it-says")
+        console.log(chalk.bgHex('#ff8000').bold.black("Please use one of the following commands:\n") + chalk.cyanBright("concert-this (artist)\nspotify-this-song (song)\nmovie-this (movie name)\ndo-what-it-says"))
         break;
 
 }
@@ -75,30 +85,45 @@ switch (command) {
 // concert function for getting artist venue information
 function concertThis(artist) {
     request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
-        if (error) { console.log('error:', error); };
+        if (error) {
+            console.log('error:', error);
+        };
         //if response is returned and status code is good to GO
         if (response && response.statusCode === 200) {
+            appendLogFile("\n~~~\nCONCERT-THIS FUNCTION: " + artist.toUpperCase() + "\n~~~\n");
             // Make body readable:
             var canRead = JSON.parse(body);
-            //get venue name
-            var venue = canRead[0].venue.name;
-            //venue location City, State
-            var venueLoc = canRead[0].venue.city + ", " + canRead[0].venue.region;
-            //format the date
-            var date = moment(canRead[0].datetime).format("MM/DD/YYYY");
-            // "MMMM DD, YYYY @ hh:mm A" -- I like this better!
+            canRead.forEach(element => {
+                //get venue name
+                var venue = element.venue.name;
+                //venue location City, State
+                var venueLoc = element.venue.city + ", " + element.venue.region;
+                //format the date
+                var date = moment(element.datetime).format("MM/DD/YYYY");
+                // "MMMM DD, YYYY @ hh:mm A" -- I like this better!
 
-            var all = "Venue Name: " + venue + "\nVenue Location: " + venueLoc + "\nDate: " + date;
-            //Log out useful info:
-            console.log(all)
-            appendLogFile("\n~~~\nCONCERT-THIS FUNCTION:\n~~~\n" + all);
+                var all = "Venue Name: " + venue +
+                    "\nVenue Location: " + venueLoc +
+                    "\nDate: " + date + "\n~\n";
+
+                var allChalk = chalk.hex('#000000').bgHex('#2299ff')("Venue Name:") + " " + chalk.hex('#2299ff')(venue) +
+                    chalk.hex('#000000').bgHex('#2299ff')("\nVenue Location:") + " " + chalk.hex('#2299ff')(venueLoc) +
+                    chalk.hex('#000000').bgHex('#2299ff')("\nDate:") + " " + chalk.hex('#2299ff')(date + "\n~\n");
+                //Log out useful info:
+                console.log(allChalk)
+                appendLogFile(all);
+
+            });
         };
     })
 }
 
 // spotify function to get song name and artist info
 function spotifyThisSong(song) {
-    spotify.search({ type: 'track', query: song }, function (err, data) {
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (err, data) {
         // error log if error
         if (err) {
             return console.log('Error occurred: ' + err);
@@ -111,12 +136,19 @@ function spotifyThisSong(song) {
         var albumName = firstResult.album.name;
         //----------------------------------------
 
-        var all = "Artist(s): " + artist + "\nSong Name: " + songName + "\nPreview URL: " + preview + "\nAlbum Name: " + albumName;
+        var all = "Artist(s): " + artist +
+            "\nSong Name: " + songName +
+            "\nPreview URL: " + preview +
+            "\nAlbum Name: " + albumName;
 
-        console.log(all)
-        appendLogFile("\n~~~\nSPOTIFY-THIS-SONG FUNCTION:\n~~~\n" + all);
-    }
-    )
+        var allChalk = chalk.hex('#000000').bgHex('#2299ff')("Artist(s):") + " " + chalk.hex('#2299ff')(artist) +
+            chalk.hex('#000000').bgHex('#2299ff')("\nSong Name: ") + " " + chalk.hex('#2299ff')(songName) +
+            chalk.hex('#000000').bgHex('#2299ff')("\nPreview URL: ") + " " + chalk.hex('#2299ff').underline(preview) +
+            chalk.hex('#000000').bgHex('#2299ff')("\nAlbum Name: ") + " " + chalk.hex('#2299ff')(albumName);
+
+        console.log(allChalk)
+        appendLogFile("\n~~~\nSPOTIFY-THIS-SONG FUNCTION: " + song.toUpperCase() + "\n~~~\n" + all);
+    })
 }
 
 // omdb function to get movie info
@@ -135,6 +167,8 @@ function movieThis(movie) {
             var movieYear = theMovie.Year;
             var imdbRating = theMovie.Ratings[0].Source + " rating: " + theMovie.Ratings[0].Value;
             var rtRating = theMovie.Ratings[1].Source + " rating: " + theMovie.Ratings[1].Value;
+            var imdbRatingChalk = chalk.hex('#000000').bgHex('#2299ff')(theMovie.Ratings[0].Source + " rating:")+" " + chalk.hex('#2299ff')(theMovie.Ratings[0].Value);
+            var rtRatingChalk = chalk.hex('#000000').bgHex('#2299ff')(theMovie.Ratings[1].Source + " rating:")+" " + chalk.hex('#2299ff')(theMovie.Ratings[1].Value);
             var country = theMovie.Country;
             var languages = theMovie.Language;
             var plot = theMovie.Plot;
@@ -148,10 +182,19 @@ function movieThis(movie) {
                 "\nLanguages: " + languages +
                 "\nPlot: " + plot +
                 "\nActors: " + actors
+                // chalk.hex('#000000').bgHex('#2299ff')("\nAlbum Name: ") + " " + chalk.hex('#2299ff')(albumName)
+            var allChalk = chalk.hex('#000000').bgHex('#2299ff')("Title:") + " " + chalk.hex('#2299ff')(movieTitle) +
+                chalk.hex('#000000').bgHex('#2299ff')("\nYear:") + " " + chalk.hex('#2299ff')(movieYear) +
+                "\n" + imdbRatingChalk +
+                "\n" + rtRatingChalk +
+                chalk.hex('#000000').bgHex('#2299ff')("\nCountry:") + " " + chalk.hex('#2299ff')(country) +
+                chalk.hex('#000000').bgHex('#2299ff')("\nLanguages:") + " " + chalk.hex('#2299ff')(languages) +
+                chalk.hex('#000000').bgHex('#2299ff')("\nPlot:") + " " + chalk.hex('#2299ff')(plot) +
+                chalk.hex('#000000').bgHex('#2299ff')("\nActors:") + " " + chalk.hex('#2299ff')(actors)
 
-            console.log(all);
+            console.log(allChalk);
 
-            appendLogFile("\n~~~\nMOVIE-THIS FUNCTION:\n~~~\n" + all);
+            appendLogFile("\n~~~\nMOVIE-THIS FUNCTION: " + movie.toUpperCase() + "\n~~~\n" + all);
         }
     });
 
